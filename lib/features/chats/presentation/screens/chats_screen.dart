3fe8018ffa3399cc75ec80db55ee828885265097
karthikeyan9952 <karthikeyan995282@gmail.com>
2023-06-19 23:34:57 +0530

    Awsome notifications

--- lib/features/chats/presentation/screens/chats_screen.dart
+++ lib/features/chats/presentation/screens/chats_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:realtime_communication_app/utilities/keys.dart';
 import 'package:realtime_communication_app/widgets/bottom_nav_bar.dart';
 
 class ChatsScreen extends StatelessWidget {
@@ -7,8 +9,26 @@ class ChatsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AppBar(automaticallyImplyLeading: false),
+      appBar: AppBar(automaticallyImplyLeading: false),
       bottomNavigationBar: const BottomNavBar(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                  onPressed: () async {
+                    String? token = await FirebaseMessaging.instance.getToken();
+                    logger.w(token);
+                  },
+                  icon: const Icon(Icons.upload)),
+              IconButton(
+                  onPressed: () async {}, icon: const Icon(Icons.download)),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -10,7 +12,49 @@ import 'firebase_options.dart';
 import 'services/theme/theme_constants.dart';
 import 'utilities/keys.dart';
 
+Future<void> backgroundMessageHandler(RemoteMessage message) async {
+  String? title = message.notification!.title,
+      body = message.notification!.body;
+  AwesomeNotifications().createNotification(
+      content: NotificationContent(
+          id: 123,
+          channelKey: "Call_channel",
+          color: Colors.white,
+          title: title,
+          body: body,
+          category: NotificationCategory.Call,
+          wakeUpScreen: true,
+          fullScreenIntent: true,
+          autoDismissible: false,
+          backgroundColor: Colors.orange),
+      actionButtons: [
+        NotificationActionButton(
+            key: "ACCEPT",
+            label: "Accept",
+            color: Colors.green,
+            autoDismissible: true),
+        NotificationActionButton(
+            key: "REJECT",
+            label: "Reject",
+            color: Colors.red,
+            autoDismissible: true)
+      ]);
+}
+
 void main() async {
+  AwesomeNotifications().initialize(null, [
+    NotificationChannel(
+        channelKey: "Call_channel",
+        channelName: "Call Channel",
+        channelDescription: "Channel of calling",
+        defaultColor: Colors.redAccent,
+        ledColor: Colors.white,
+        importance: NotificationImportance.Max,
+        channelShowBadge: true,
+        locked: true,
+        defaultRingtoneType: DefaultRingtoneType.Ringtone)
+  ]);
+  FirebaseMessaging.onBackgroundMessage(backgroundMessageHandler);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
